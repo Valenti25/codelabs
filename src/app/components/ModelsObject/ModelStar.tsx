d481434413e4f835b4b1f418a87562ac356a3b1f
Valenti25 <161212070+Valenti25@users.noreply.github.com>
2025-09-21 20:19:49 +0700

    Update ModelStar.tsx

--- src/app/components/ModelsObject/ModelStar.tsx
+++ src/app/components/ModelsObject/ModelStar.tsx
@@ -33,7 +33,7 @@ export default function ModelCanvas() {
       ref={sectionRef}
       className="relative h-[25vh] w-full lg:min-h-[500px] lg:overflow-hidden"
     >
-      <div className="absolute top-[90%] z-[-10] -left-10 h-4/5 w-1/2 lg:top-[30%] lg:-left-24">
+      <div className="absolute top-[70%] z-[-10] -left-10 h-4/5 w-1/2 lg:top-[40%] lg:-left-24">
         {isInView && (
           <Suspense>
             <Spline scene="https://prod.spline.design/dIHvXSNd1BJQN7MA/scene.splinecode" />
